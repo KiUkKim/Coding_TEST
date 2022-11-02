3eba0f9e97896fbd77e330cdf26fe2dc9ce09d05
KiUkKim <zidh1@korea.ac.kr>
2022-11-02 21:39:44 +0900

    BOJ 12852 1로만들기 2 S1

---
+++ DynamicPrograming/BOJ 12852 1로 만들기 2 S1.cpp
@@ -0,0 +1,28 @@
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+int n;
+
+int dp[3][100001];
+
+int main()
+{
+    fastio;
+
+    cin >> n;
+
+    dp[]
+
+    for(int i = 0; i < n; i++)
+    {
+
+    }
+
+    return 0;
+}
